0a462dc703beb5187619285efa3323bf9186a9aa
maxim <aalefpeneash@gmail.com>
2018-07-23 13:41:13 +0300

    second

--- web/features/step_defifnitions/kafka_steps.js
+++ web/features/step_defifnitions/kafka_steps.js
@@ -15,19 +15,30 @@ Given('the producer and consumer', () =>  {
 
 Then('the consumer fetches message {string} {string}',  function(message, expected) {
     let check = true; 
-    for (let index = 0; index < 2; index++) {
-        // pc.sendMessage(message);
-        // return pc.fetchMessage().then((res) => {
-        //     return assert.equal(expected, res);
-        // });   
-        let a = Math.floor(Math.random() * 10) % 2;
-        console.log(a);  
-        if (a == 1){
-            check = false;
-            break;
-        } 
-    }
-    assert(check);
+    let a;
+    let promise = new Promise((resolve, reject) => {
+        for (let index = 0; index < 2; index++) {
+            pc.sendMessage(message);
+            pc.fetchMessage().then((res) => {
+                // assert.equal(expected, res);
+            
+                a = Math.floor(Math.random() * 10) % 2;
+                console.log(a + "|");
+                if (a == 1){
+                    check = false;
+                    // break;
+                } 
+                console.log("|" + check);
+            });
+            if(index == 1){
+                resolve(check);                
+            }
+        }
+    });
+    
+    promise.then((c) => {
+        assert(c);
+    });
 });
 
 //callback way 
